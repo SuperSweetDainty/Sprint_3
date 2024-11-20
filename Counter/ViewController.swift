cf06ec43b8c4912c684c2e268e8547bf739389bb
daniil <danya.laputin@mail.ru>
2024-11-21 01:10:21 +0300

    Базовая задача, спринт 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var buttonLabelChanger: UIButton! // связь счётчика и кода
 
+    @IBOutlet weak var labelCounter: UILabel! // связь кнопки счётчика и кода
+  
+    var counter = 0 //переменная показателя счётчика
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        labelCounter.text = "Значение счётчика: \(counter)" // добавление переменной показателя счётчика в  сам счётчик
     }
 
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1 // Увеличение переменной счётчика
+        labelCounter.text = "Значение счётчика: \(counter)" // вывод обновлённого счётчика
+    }
 }
 
