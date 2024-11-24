402a15ec17d2ceacb0b54bc138f0a18815948384
daniil <danya.laputin@mail.ru>
2024-11-24 16:28:42 +0300

    Внесены рекомендуемые корректировки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,22 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
- 
-    @IBOutlet weak var buttonLabelChanger: UIButton! // связь счётчика и кода
-
-    @IBOutlet weak var labelCounter: UILabel! // связь кнопки счётчика и кода
-  
-    var counter = 0 //переменная показателя счётчика
-   
+private final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var buttonLabelChanger: UIButton! // связь счётчика и кода
+    
+    @IBOutlet private weak var labelCounter: UILabel! // связь кнопки счётчика и кода
+    
+    private var counter: Int = .zero //переменная показателя счётчика
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         labelCounter.text = "Значение счётчика: \(counter)" // добавление переменной показателя счётчика в  сам счётчик
     }
-
-
-    @IBAction func buttonDidTap(_ sender: Any) {
+    
+    
+    @IBAction private func buttonDidTap(_ sender: Any) {
         counter += 1 // Увеличение переменной счётчика
         labelCounter.text = "Значение счётчика: \(counter)" // вывод обновлённого счётчика
     }
